2854cbbf3613099b7f41b6ae4dbf1347d01c72c3
Ibrahim <abo.mahroq@gmail.com>
2022-07-19 08:44:48 +0300

    Add project files.

---
+++ Pharmacy/Controllers/AuthorController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pharmacy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pharmacy.Controllers
+{
+    [Route("api/Author")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+
+        public static List<Author> Authors = new List<Author>() {
+                new Author(){ Id = 1,Name  = "Ibrahim" ,Location=   "Gaza"},
+                new Author(){Id = 2,Name  = "Ahmed" ,Location=   "Gaza"},
+                new Author(){Id = 3,Name  = "Ali" ,Location=   "Gaza"},
+            };
+
+
+        [HttpGet]
+        public List<Author> getAllAuthors()
+        {
+            return Authors;
+        }
+
+
+        [HttpGet("{Id}")]
+        public IActionResult GetAuthor(int Id)
+        {
+            var currentUser = Authors.Where(x => x.Id == Id).FirstOrDefault();
+            if (currentUser == null){
+                return NotFound(new { code = 320 , message = "Invalid Id"});
+            }
+            else {
+                return Ok(currentUser);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult AddAuthor(Author author)
+        {
+            if (String.IsNullOrWhiteSpace(author.Name))
+            {
+
+            }
+            Authors.Add(author);
+            return Ok();
+        }
+
+    }
+}
---
+++ Pharmacy/Models/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pharmacy.Models
+{
+    public class Author
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Location { get; set; }
+
+
+    }
+}
